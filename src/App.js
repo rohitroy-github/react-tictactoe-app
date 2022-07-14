ec25d6e3b5f2ed9d973fa4d0cff008b6eebd9524
rohit <rohitdevops.roy@gmail.com>
2022-07-14 17:56:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-header'> 
+    <p className='heading-text'> 
+    React Tic Tac Toe App</p> 
+    <div> 
+      X 
     </div>
-  );
+
+    <div> 
+      X 
+    </div>
+
+    <div> 
+      X 
+    </div>
+    </div>
+    )
 }
 
-export default App;
+export default App
